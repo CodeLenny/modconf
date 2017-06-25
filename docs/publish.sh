783885da15d3454b622089dbe8f073f18ac80a4f
Ryan Leonard <rhinoisnot@gmail.com>
2017-06-25 11:42:37 -0400

    Shortened syntax.

--- docs/publish.sh
+++ docs/publish.sh
@@ -4,7 +4,7 @@ set -e
 PUBLIC_SURGE_USERNAME=$(echo -n "$SURGE_LOGIN" | sed "s/^[^@]*/${SURGE_LOGIN:0:1}***/")
 LAST_COMMIT="$(git log -1 --pretty=%B)"
 
-if [[ "$CI" == "true" ]] && [[ "$TRAVIS_BRANCH" != "master" ]] && [[ "$LAST_COMMIT" != *"[deploy]"* ]]; then
+if $CI && [[ "$TRAVIS_BRANCH" != "master" ]] && [[ "$LAST_COMMIT" != *"[deploy]"* ]]; then
   echo "Not on 'master'.  Aborting deployment."
   echo "  Add '[deploy]' to the commit message to override deployment check."
   exit 0;
