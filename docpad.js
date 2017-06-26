de3a58b3c2d685229652b903274c9c4eb9f12176
Ryan Leonard <rhinoisnot@gmail.com>
2017-06-26 07:49:29 -0400

    Added docpad compilation.

---
+++ docpad.js
@@ -0,0 +1,39 @@
+module.exports = {
+
+  outPath: "public",
+
+  srcPath: "docs",
+
+  documentPaths: [
+    "documents",
+    "README.adoc",
+  ],
+
+  collections: {
+
+    adoc: function() {
+      return this
+        .getCollection("documents")
+        .findAllLive({ extensions: { $in: [ "adoc" ] } })
+        .on("add", (model) => model.setMetaDefaults({
+          layout: "adoc"
+        }));
+    },
+
+    noRender: function() {
+      return this
+        .getCollection("documents")
+        .findAllLive({ outFilename: { $in: [
+          "README.adoc",
+        ]}})
+        .on("add", (model) => model.setMetaDefaults({
+          render: false,
+          outFilename: model.attributes.filename,
+          contentRendered: model.attributes.body,
+          rendered: true,
+        }));
+    },
+
+  },
+
+}
